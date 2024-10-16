b3a45a44ad8fcfec02a2e022f60ca4bffa0ae491
xcDoza <xc2doza@hotmail.com>
2024-10-16 17:56:53 -0400

    :sparkles: add list class

--- src/com/sucursales/Lista.java
+++ src/com/sucursales/Lista.java
@@ -9,5 +9,47 @@ package com.sucursales;
  * @author xc2do
  */
 public class Lista {
-    
+
+    private Nodo head, tail;
+    private int size;
+
+    public Lista() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public Nodo getHead() {
+        return head;
+    }
+
+    public void setHead(Nodo head) {
+        this.head = head;
+    }
+
+    public Nodo getTail() {
+        return tail;
+    }
+
+    public void setTail(Nodo tail) {
+        this.tail = tail;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    //Mostrar los elementos de la lista
+    public void mostrar() {
+        Nodo actual = head;
+        while (actual != null) {
+            System.out.println(actual.getElement());
+            actual = actual.getNext();
+        }
+    }
+
 }
