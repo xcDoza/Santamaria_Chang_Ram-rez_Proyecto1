866a67c0e2723c539ae0d38a0cda63bb0804ee60
xcDoza <xc2doza@hotmail.com>
2024-10-16 18:39:27 -0400

    :sparkles: add estacion and red de transporte

--- src/com/sucursales/Estacion.java
+++ src/com/sucursales/Estacion.java
@@ -9,5 +9,40 @@ package com.sucursales;
  * @author xc2do
  */
 public class Estacion {
-    
+
+    private String nombre, conexion;
+
+    public Estacion(String nombre) {
+        this.nombre = nombre;
+        this.conexion = null;
+    }
+
+    public Estacion(String nombre, String conexion) {
+        this.nombre = nombre;
+        this.conexion = conexion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(String conexion) {
+        this.conexion = conexion;
+    }
+
+    @Override
+    public String toString() {
+        if (conexion != null) {
+            return nombre + " con conexión a " + conexion;
+        }
+        return nombre;
+    }
 }
--- src/com/sucursales/RedDeTransporte.java
+++ src/com/sucursales/RedDeTransporte.java
@@ -9,5 +9,37 @@ package com.sucursales;
  * @author xc2do
  */
 public class RedDeTransporte {
-    
+
+    private String nombreRed;
+    private Lista<Linea> lineas;
+
+    public RedDeTransporte(String nombreRed) {
+        this.nombreRed = nombreRed;
+        this.lineas = new Lista<>();
+    }
+
+    public String getNombreRed() {
+        return nombreRed;
+    }
+
+    public void setNombreRed(String nombreRed) {
+        this.nombreRed = nombreRed;
+    }
+
+    public Lista<Linea> getLineas() {
+        return lineas;
+    }
+
+    public void setLineas(Lista<Linea> lineas) {
+        this.lineas = lineas;
+    }
+
+    public void agregarLinea(Linea linea) {
+        lineas.agregar(linea);
+    }
+
+    @Override
+    public String toString() {
+        return "Red: " + nombreRed + ", Líneas: " + lineas;
+    }
 }
